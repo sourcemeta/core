395af655ae72977637bde2e1549607e7656051b8
Juan Cruz Viotti <jv@jviotti.com>
2025-04-18 12:52:42 -0400

    Don't enforce `wrap()` URIs to be absolute (they could be relative)
    See: https://github.com/sourcemeta/jsonschema/issues/304#issuecomment-2814383860
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/jsonschema/jsonschema_wrap_test.cc
+++ test/jsonschema/jsonschema_wrap_test.cc
@@ -15,6 +15,20 @@ TEST(JSONSchema_wrap, identifier_without_fragment) {
   EXPECT_EQ(result, expected);
 }
 
+// Should still work, as people might be relying on relative URIs
+// as identifiers though still correctly resolving those
+TEST(JSONSchema_wrap, identifier_relative) {
+  const auto identifier{"/foo/bar"};
+  const auto result{sourcemeta::core::wrap(identifier)};
+
+  const auto expected{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$ref": "/foo/bar"
+  })JSON")};
+
+  EXPECT_EQ(result, expected);
+}
+
 TEST(JSONSchema_wrap, identifier_with_fragment) {
   const auto identifier{"https://www.example.com#/foo/bar"};
   const auto result{sourcemeta::core::wrap(identifier)};
