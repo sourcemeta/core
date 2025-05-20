821218d5b19a459f71709847e2fd5e2a6ec42c4e
Juan Cruz Viotti <jv@jviotti.com>
2025-05-20 13:27:18 -0300

    Support constructing `std::map`s of `URI` (#1649)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/uri/uri_test.cc
+++ test/uri/uri_test.cc
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 #include <sourcemeta/core/uri.h>
 
+#include <map>
+
 TEST(URI, copy_constructor) {
   const sourcemeta::core::URI uri_1{"https://example.com"};
   const sourcemeta::core::URI uri_2{uri_1};
@@ -25,4 +27,27 @@ TEST(URI, using_istream) {
   std::istringstream input{"https://example.com"};
   const sourcemeta::core::URI uri{input};
   EXPECT_EQ(uri.recompose(), "https://example.com");
+}
+
+TEST(URI, equality) {
+  EXPECT_EQ(sourcemeta::core::URI{"https://example.com"},
+            sourcemeta::core::URI{"https://example.com"});
+  EXPECT_NE(sourcemeta::core::URI{"https://example.com"},
+            sourcemeta::core::URI{"http://example.com"});
+  const sourcemeta::core::URI self{"https://example.com"};
+  EXPECT_EQ(self, self);
+}
+
+TEST(URI, map) {
+  std::map<sourcemeta::core::URI, bool> map;
+  map.emplace("https://example.com", true);
+  map.emplace("https://foo.com", false);
+
+  EXPECT_EQ(map.size(), 2);
+  EXPECT_TRUE(map.contains(sourcemeta::core::URI{"https://example.com"}));
+  EXPECT_TRUE(map.contains(sourcemeta::core::URI{"https://foo.com"}));
+  EXPECT_FALSE(map.contains(sourcemeta::core::URI{"https://example.org"}));
+  EXPECT_FALSE(map.contains(sourcemeta::core::URI{"https://foo.org"}));
+  EXPECT_TRUE(map.at(sourcemeta::core::URI{"https://example.com"}));
+  EXPECT_FALSE(map.at(sourcemeta::core::URI{"https://foo.com"}));
 }
