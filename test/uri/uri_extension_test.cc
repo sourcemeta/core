c5a0a206397fce6e1be9c7576e464b08bbced428
Juan Cruz Viotti <jv@jviotti.com>
2025-06-02 18:26:42 -0300

    Implement a `URI::extension` method to set path extensions (#1715)
    Fixes: https://github.com/sourcemeta/core/issues/1714
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/uri/uri_extension_test.cc
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+
+#include <sourcemeta/core/uri.h>
+
+#include <sstream>
+
+TEST(URI_extension, absolute_single_component_path_without_period) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com/foo"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com/foo.json");
+}
+
+TEST(URI_extension, absolute_single_component_path_with_period) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com/foo"};
+  uri.extension(".json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com/foo.json");
+}
+
+TEST(URI_extension, absolute_single_component_path_with_extension) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com/foo.xml"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com/foo.json");
+}
+
+TEST(URI_extension, absolute_single_component_path_with_double_extension) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com/foo.tar.gz"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com/foo.tar.json");
+}
+
+TEST(URI_extension, absolute_no_path) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com");
+}
+
+TEST(URI_extension, absolute_trailing_slash) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com/"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com/");
+}
+
+TEST(URI_extension, absolute_trailing_slashes) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com////"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com////");
+}
+
+TEST(URI_extension, absolute_trailing_period) {
+  sourcemeta::core::URI uri{"https://www.sourcemeta.com/foo."};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "https://www.sourcemeta.com/foo.json");
+}
+
+TEST(URI_extension, fragment_only) {
+  sourcemeta::core::URI uri{"#foo"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "#foo");
+}
+
+TEST(URI_extension, relative_multi_component_1) {
+  sourcemeta::core::URI uri{"/foo/bar"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "/foo/bar.json");
+}
+
+TEST(URI_extension, relative_multi_component_2) {
+  sourcemeta::core::URI uri{"foo/bar"};
+  uri.extension("json");
+  EXPECT_EQ(uri.recompose(), "foo/bar.json");
+}
