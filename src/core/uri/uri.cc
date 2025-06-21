9715a94efeec4bfd3ba94dc73d1d2b7569c02063
Balakrishna Avulapati <ba@bavulapati.com>
2025-06-21 16:20:46 +0530

    remove the comment
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- src/core/uri/uri.cc
+++ src/core/uri/uri.cc
@@ -141,7 +141,6 @@ URI::URI(const URI &other) : URI{other.recompose()} {}
 URI::URI(URI &&other)
     : parsed(other.parsed), data{std::move(other.data)},
       internal{std::move(other.internal)} {
-  // this->parsed = other.parsed;
   this->path_ = std::move(other.path_);
   this->scheme_ = std::move(other.scheme_);
   this->userinfo_ = std::move(other.userinfo_);
