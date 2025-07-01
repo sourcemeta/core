7ed2136bbacb1ecc94b7245890edfa0bbc27f195
Juan Cruz Viotti <jv@jviotti.com>
2025-07-01 09:42:27 -0400

    Implement a GZIP library (powered by ZLIB)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/gzip/gzip_test.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+#include <sourcemeta/core/gzip.h>
+
+TEST(GZIP, compress_string_1) {
+  const auto result{sourcemeta::core::gzip("Hello World")};
+  EXPECT_TRUE(result.has_value());
+  EXPECT_EQ(result.value().size(), 31);
+}
