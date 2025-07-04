e9369fe0083f7e34186a09759973d9cd5bfbc641
Balakrishna Avulapati <ba@bavulapati.com>
2025-07-04 16:19:12 +0530

    Error out if clang-tidy is invoked without clang tool chain
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- cmake/common/targets/clang-tidy.cmake
+++ cmake/common/targets/clang-tidy.cmake
@@ -1,6 +1,11 @@
 function(sourcemeta_enable_clang_tidy)
-  find_program(CLANG_TIDY_BIN NAMES clang-tidy REQUIRED)
-  set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_BIN};--config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;-header-filter=${CMAKE_CURRENT_SOURCE_DIR}/src/*" PARENT_SCOPE)
+  if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    find_program(CLANG_TIDY_BIN NAMES clang-tidy REQUIRED)
+    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_BIN};--config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;-header-filter=${CMAKE_CURRENT_SOURCE_DIR}/src/*" PARENT_SCOPE)
+  else()
+    message(FATAL_ERROR "Incompatible compiler toolchain. Clang-tidy is only tested to be working with clang toolchain")
+  endif()
+
 endfunction()
 
 function(sourcemeta_disable_clang_tidy)
