5a00ecac6c169d7aa6d502378253652c2ca49cdd
Balakrishna Avulapati <ba@bavulapati.com>
2025-07-04 16:08:29 +0530

    make clang-tidy required on ci job
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- cmake/common/targets/clang-tidy.cmake
+++ cmake/common/targets/clang-tidy.cmake
@@ -1,5 +1,5 @@
 function(sourcemeta_enable_clang_tidy)
-  find_program(CLANG_TIDY_BIN NAMES clang-tidy)
+  find_program(CLANG_TIDY_BIN NAMES clang-tidy REQUIRED)
   set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_BIN};--config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;-header-filter=${CMAKE_CURRENT_SOURCE_DIR}/src/*" PARENT_SCOPE)
 endfunction()
 
