26d0bbc502faf17dba5e8cae68ce55bd82176ab3
Balakrishna Avulapati <ba@bavulapati.com>
2025-07-08 13:41:01 +0530

    pass header-filter as a cmake function parameter
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- cmake/common/targets/clang-tidy.cmake
+++ cmake/common/targets/clang-tidy.cmake
@@ -1,9 +1,18 @@
 function(sourcemeta_find_clang_tidy)
   if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     find_program(CLANG_TIDY_BIN NAMES clang-tidy REQUIRED)
-    set(SOURCEMETA_CXX_CLANG_TIDY "${CLANG_TIDY_BIN};--config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;-header-filter=${CMAKE_CURRENT_SOURCE_DIR}/src/*" PARENT_SCOPE)
   else()
     message(FATAL_ERROR "Incompatible compiler toolchain. Clang-tidy is only tested to be working with clang toolchain")
   endif()
 
+endfunction()
+
+function(sourcemeta_set_clang_tidy_config)
+  cmake_parse_arguments(SOURCEMETA_SET_CLANG_TIDY_CONFIG "" "HEADER_FILTER" "" ${ARGN})
+  if(NOT SOURCEMETA_SET_CLANG_TIDY_CONFIG_HEADER_FILTER)
+    message(FATAL_ERROR "HEADER_FILTER is required for clang-tidy.")
+  endif()
+
+  sourcemeta_find_clang_tidy()
+  set(SOURCEMETA_CXX_CLANG_TIDY "${CLANG_TIDY_BIN};--config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;-header-filter=${SOURCEMETA_SET_CLANG_TIDY_CONFIG_HEADER_FILTER}" PARENT_SCOPE)
 endfunction()
