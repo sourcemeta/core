847cea1a1a8c11ae118cbb3bf8e7d8953184f9aa
Balakrishna Avulapati <ba@bavulapati.com>
2025-07-08 12:35:22 +0530

    enable clang-tidy at target level
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- cmake/common/targets/clang-tidy.cmake
+++ cmake/common/targets/clang-tidy.cmake
@@ -1,13 +1,9 @@
-function(sourcemeta_enable_clang_tidy)
-  if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+function(sourcemeta_find_clang_tidy)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     find_program(CLANG_TIDY_BIN NAMES clang-tidy REQUIRED)
-    set(CMAKE_CXX_CLANG_TIDY "${CLANG_TIDY_BIN};--config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;-header-filter=${CMAKE_CURRENT_SOURCE_DIR}/src/*" PARENT_SCOPE)
+    set(SOURCEMETA_CXX_CLANG_TIDY "${CLANG_TIDY_BIN};--config-file=${PROJECT_SOURCE_DIR}/.clang-tidy;-header-filter=${CMAKE_CURRENT_SOURCE_DIR}/src/*" PARENT_SCOPE)
   else()
     message(FATAL_ERROR "Incompatible compiler toolchain. Clang-tidy is only tested to be working with clang toolchain")
   endif()
 
-endfunction()
-
-function(sourcemeta_disable_clang_tidy)
-  unset(CMAKE_CXX_CLANG_TIDY PARENT_SCOPE)
 endfunction()
