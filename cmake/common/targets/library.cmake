734416442f77c708485d77a74fb4bef621d86f16
Balakrishna Avulapati <ba@bavulapati.com>
2025-07-18 16:21:01 +0530

    enable clang-tidy from sourcemeta_library() definition
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- cmake/common/targets/library.cmake
+++ cmake/common/targets/library.cmake
@@ -115,6 +115,9 @@ function(sourcemeta_library)
     target_include_directories(${TARGET_NAME}
       PUBLIC "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}/include>")
   endif()
+
+  sourcemeta_clang_tidy_attempt_enable(TARGET_NAME "${TARGET_NAME}")
+
 endfunction()
 
 function(sourcemeta_library_install)
