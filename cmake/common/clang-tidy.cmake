c2d3eee18f3711cad989c05f77187ae3c7de6632
Balakrishna Avulapati <ba@bavulapati.com>
2025-07-19 11:55:22 +0530

    try
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- cmake/common/clang-tidy.cmake
+++ cmake/common/clang-tidy.cmake
@@ -109,6 +109,8 @@ function(sourcemeta_clang_tidy_attempt_enable)
 
   set(CMAKE_CXX_CLANG_TIDY
       "${CLANG_TIDY_BIN};--config-file=${CLANG_TIDY_CONFIG};-header-filter=${PROJECT_SOURCE_DIR}/src/*"
+      "--extra-arg=-isystemroot"
+      "--extra-arg=${MACOSX_RESOURCE_PATH}"
       "--extra-arg=-isystem"
       "--extra-arg=${MACOSX_RESOURCE_PATH}/include/"
       PARENT_SCOPE)
