1a2cd39241db6b204dc3f9c63d3e4758ffd10e05
Juan Cruz Viotti <jv@jviotti.com>
2025-07-24 16:06:24 -0400

    Refactor GZIP module to offer a full stream interface (#1887)
    Fixes: https://github.com/sourcemeta/core/issues/1886
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/gzip/gzip_test.cc
+++ test/gzip/gzip_test.cc
@@ -4,20 +4,50 @@
 
 #include <sstream>
 
-TEST(GZIP, compress_string_1) {
+TEST(GZIP, compress_stream_1) {
+  const auto value{"Hello World"};
+  std::istringstream input{value};
+  std::ostringstream output;
+  sourcemeta::core::gzip(input, output);
+  EXPECT_EQ(output.str().size(), 31);
+}
+
+TEST(GZIP, compress_stream_to_string_1) {
+  const auto value{"Hello World"};
+  std::istringstream input{value};
+  const auto result{sourcemeta::core::gzip(input)};
+  EXPECT_EQ(result.size(), 31);
+}
+
+TEST(GZIP, compress_string_to_string_1) {
   const auto result{sourcemeta::core::gzip("Hello World")};
   EXPECT_EQ(result.size(), 31);
 }
 
-TEST(GZIP, decompress_string_stream_1) {
+TEST(GZIP, decompress_stream_1) {
   const auto value{"Hello World"};
-  const auto input{sourcemeta::core::gzip(value)};
-  std::istringstream stream{input};
-  const auto result{sourcemeta::core::gunzip(stream)};
+  std::istringstream input{sourcemeta::core::gzip(value)};
+  std::ostringstream output;
+  sourcemeta::core::gunzip(input, output);
+  EXPECT_EQ(output.str(), value);
+}
+
+TEST(GZIP, decompress_stream_error_1) {
+  std::istringstream input{"not-zlib-content"};
+  std::ostringstream output;
+  EXPECT_THROW(sourcemeta::core::gunzip(input, output),
+               sourcemeta::core::GZIPError);
+}
+
+TEST(GZIP, decompress_stream_to_string_1) {
+  const auto value{"Hello World"};
+  std::istringstream input{sourcemeta::core::gzip(value)};
+  const auto result{sourcemeta::core::gunzip(input)};
   EXPECT_EQ(result, value);
 }
 
-TEST(GZIP, decompress_string_stream_error_1) {
-  std::istringstream stream{"not-zlib-content"};
-  EXPECT_THROW(sourcemeta::core::gunzip(stream), sourcemeta::core::GZIPError);
+TEST(GZIP, decompress_string_to_string_1) {
+  const auto value{"Hello World"};
+  const auto result{sourcemeta::core::gunzip(sourcemeta::core::gzip(value))};
+  EXPECT_EQ(result, value);
 }
