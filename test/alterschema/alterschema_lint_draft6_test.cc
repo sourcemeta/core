59e20165a94d6fa3aeb5a00e8f0b2096b214deeb
Juan Cruz Viotti <jv@jviotti.com>
2025-08-07 15:51:47 -0400

    Describe specific paths where unnecessary `allOf` wrapper applies
    See: https://github.com/sourcemeta/jsonschema/issues/424
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/alterschema/alterschema_lint_draft6_test.cc
+++ test/alterschema/alterschema_lint_draft6_test.cc
@@ -1297,6 +1297,25 @@ TEST(AlterSchema_lint_draft6, unnecessary_allof_wrapper_1) {
   EXPECT_EQ(document, expected);
 }
 
+TEST(AlterSchema_lint_draft6, unnecessary_allof_wrapper_2) {
+  const sourcemeta::core::JSON document = sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "allOf": [
+      { "type": "string" }
+    ]
+  })JSON");
+
+  LINT_WITHOUT_FIX_FOR_READABILITY(document, result, traces);
+
+  EXPECT_FALSE(result.first);
+  EXPECT_EQ(traces.size(), 1);
+  EXPECT_LINT_TRACE(
+      traces, 0, "", "unnecessary_allof_wrapper_draft",
+      "Wrapping any keyword other than `$ref` in `allOf` is unnecessary and "
+      "may even introduce a minor evaluation performance overhead",
+      "- /allOf/0/type\n");
+}
+
 TEST(AlterSchema_lint_draft6, multiple_of_default_1) {
   sourcemeta::core::JSON document = sourcemeta::core::parse_json(R"JSON({
     "$schema": "http://json-schema.org/draft-06/schema#",
