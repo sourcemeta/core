704b199c386bf3d85f76f1563af0a46b8b06b91b
karan-palan <karanpalan007@gmail.com>
2025-08-18 11:19:56 +0530

    use std:all_of
    Signed-off-by: karan-palan <karanpalan007@gmail.com>

--- src/extension/alterschema/linter/then_false.h
+++ src/extension/alterschema/linter/then_false.h
@@ -10,43 +10,30 @@ public:
             const SchemaFrame &, const SchemaFrame::Location &,
             const SchemaWalker &, const SchemaResolver &) const
       -> SchemaTransformRule::Result override {
-    if (!contains_any(vocabularies,
-                      {"https://json-schema.org/draft/2020-12/vocab/applicator",
-                       "https://json-schema.org/draft/2019-09/vocab/applicator",
-                       "http://json-schema.org/draft-07/schema#"})) {
-      return false;
-    }
-
-    if (!schema.is_object() || !schema.defines("if") ||
-        !schema.defines("then") || !is_schema(schema.at("then")) ||
-        !schema.at("then").is_boolean() || schema.at("then").to_boolean() ||
-        !is_schema(schema.at("if"))) {
-      return false;
-    }
-
-    if (schema.defines("else") &&
-        (!schema.at("else").is_boolean() || !schema.at("else").to_boolean())) {
-      return false;
-    }
-
-    if (schema.at("if").is_object()) {
-      for (const auto &entry : schema.at("if").as_object()) {
-        if (schema.defines(entry.first)) {
-          return false;
-        }
-      }
-    }
-
-    return true;
+    return contains_any(
+               vocabularies,
+               {"https://json-schema.org/draft/2020-12/vocab/applicator",
+                "https://json-schema.org/draft/2019-09/vocab/applicator",
+                "http://json-schema.org/draft-07/schema#"}) &&
+           schema.is_object() && schema.defines("if") &&
+           schema.defines("then") && is_schema(schema.at("then")) &&
+           schema.at("then").is_boolean() && !schema.at("then").to_boolean() &&
+           is_schema(schema.at("if")) &&
+           (!schema.defines("else") || (schema.at("else").is_boolean() &&
+                                        schema.at("else").to_boolean())) &&
+           (!schema.at("if").is_object() ||
+            std::all_of(schema.at("if").as_object().begin(),
+                        schema.at("if").as_object().end(),
+                        [&schema](const auto &entry) {
+                          return !schema.defines(entry.first);
+                        }));
   }
 
   auto transform(JSON &schema) const -> void override {
     auto if_schema = schema.at("if");
     schema.erase("if");
     schema.erase("then");
-    if (schema.defines("else")) {
-      schema.erase("else");
-    }
+    schema.erase("else");
     schema.assign("not", std::move(if_schema));
   }
 };
