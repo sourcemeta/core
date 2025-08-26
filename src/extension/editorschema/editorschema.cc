938c3fe5d3af47a00d80efb25b70e77d322ae1b1
Juan Cruz Viotti <jv@jviotti.com>
2025-08-26 11:05:24 -0400

    Default to base dialect URIs in `for_editor`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/extension/editorschema/editorschema.cc
+++ src/extension/editorschema/editorschema.cc
@@ -26,33 +26,49 @@ auto for_editor(JSON &schema, const SchemaWalker &walker,
       continue;
     }
 
+    // Get rid of the keywords we don't want anymore
     anonymize(subschema, entry.second.base_dialect);
-
     const auto vocabularies{frame.vocabularies(entry.second, resolver)};
-
     if (vocabularies.contains(
             "https://json-schema.org/draft/2020-12/vocab/core")) {
-      subschema.erase("$vocabulary");
-      subschema.erase("$anchor");
-      subschema.erase("$dynamicAnchor");
-    }
-
-    if (vocabularies.contains(
-            "https://json-schema.org/draft/2019-09/vocab/core")) {
-      subschema.erase("$vocabulary");
-      subschema.erase("$anchor");
-      subschema.erase("$recursiveAnchor");
+      subschema.erase_keys({"$vocabulary", "$anchor", "$dynamicAnchor"});
+    } else if (vocabularies.contains(
+                   "https://json-schema.org/draft/2019-09/vocab/core")) {
+      subschema.erase_keys({"$vocabulary", "$anchor", "$recursiveAnchor"});
     }
   }
 
-  // (4) Fix-up reference based on pointers from the root
+  // (4) Fix-up static references based on pointers from the root
   for (const auto &[key, reference] : frame.references()) {
+    if (key.first != SchemaReferenceType::Static) {
+      continue;
+    }
+
+    assert(!key.second.empty());
+    assert(key.second.back().is_property());
+    const auto &keyword{key.second.back().to_property()};
+
+    // The `$schema` keyword is not allowed to take relative URIs (for example,
+    // pointers going from the root). Because we remove identifiers, the only
+    // sane thing we can do here is default it to the base dialect, which
+    // editors will likely understand
+    if (keyword == "$schema") {
+      const auto uri{frame.uri(key.second)};
+      assert(uri.has_value());
+      const auto origin{frame.traverse(uri.value().get())};
+      assert(origin.has_value());
+      set(schema, key.second, JSON{origin.value().get().base_dialect});
+      continue;
+    }
+
+    // As we get rid of identifiers, we rephrase every reference to be the URI
+    // representation of the JSON Pointer to the destination from the root
     const auto result{frame.traverse(reference.destination)};
     if (result.has_value()) {
       set(schema, key.second,
           JSON{to_uri(result.value().get().pointer).recompose()});
-    } else if (!key.second.empty() && key.second.back().is_property() &&
-               key.second.back().to_property() != "$schema") {
+
+    } else {
       set(schema, key.second, JSON{reference.destination});
     }
   }
--- test/editorschema/editorschema_test.cc
+++ test/editorschema/editorschema_test.cc
@@ -250,11 +250,11 @@ TEST(EditorSchema, 2020_12_bundle_metaschema) {
                                test_resolver_2020_12);
 
   const auto expected = sourcemeta::core::parse_json(R"JSON({
-    "$schema": "#/$defs/https:~1~1example.com~1meta~11.json",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
     "type": "string",
     "$defs": {
       "https://example.com/meta/1.json": {
-        "$schema": "#/$defs/https:~1~1example.com~1meta~12.json"
+        "$schema": "https://json-schema.org/draft/2020-12/schema"
       },
       "https://example.com/meta/2.json": {
         "$schema": "https://json-schema.org/draft/2020-12/schema"
@@ -327,11 +327,11 @@ TEST(EditorSchema, 2019_09_bundle_metaschema) {
                                test_resolver_2019_09);
 
   const auto expected = sourcemeta::core::parse_json(R"JSON({
-    "$schema": "#/$defs/https:~1~1example.com~1meta~11.json",
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
     "type": "string",
     "$defs": {
       "https://example.com/meta/1.json": {
-        "$schema": "#/$defs/https:~1~1example.com~1meta~12.json"
+        "$schema": "https://json-schema.org/draft/2019-09/schema"
       },
       "https://example.com/meta/2.json": {
         "$schema": "https://json-schema.org/draft/2019-09/schema"
@@ -388,11 +388,11 @@ TEST(EditorSchema, draft7_bundle_metaschema) {
       document, sourcemeta::core::schema_official_walker, test_resolver_draft7);
 
   const auto expected = sourcemeta::core::parse_json(R"JSON({
-    "$schema": "#/definitions/https:~1~1example.com~1meta~11.json",
+    "$schema": "http://json-schema.org/draft-07/schema#",
     "type": "string",
     "definitions": {
       "https://example.com/meta/1.json": {
-        "$schema": "#/definitions/https:~1~1example.com~1meta~12.json"
+        "$schema": "http://json-schema.org/draft-07/schema#"
       },
       "https://example.com/meta/2.json": {
         "$schema": "http://json-schema.org/draft-07/schema#"
@@ -449,11 +449,11 @@ TEST(EditorSchema, draft6_bundle_metaschema) {
       document, sourcemeta::core::schema_official_walker, test_resolver_draft6);
 
   const auto expected = sourcemeta::core::parse_json(R"JSON({
-    "$schema": "#/definitions/https:~1~1example.com~1meta~11.json",
+    "$schema": "http://json-schema.org/draft-06/schema#",
     "type": "string",
     "definitions": {
       "https://example.com/meta/1.json": {
-        "$schema": "#/definitions/https:~1~1example.com~1meta~12.json"
+        "$schema": "http://json-schema.org/draft-06/schema#"
       },
       "https://example.com/meta/2.json": {
         "$schema": "http://json-schema.org/draft-06/schema#"
@@ -510,11 +510,11 @@ TEST(EditorSchema, draft4_bundle_metaschema) {
       document, sourcemeta::core::schema_official_walker, test_resolver_draft4);
 
   const auto expected = sourcemeta::core::parse_json(R"JSON({
-    "$schema": "#/definitions/https:~1~1example.com~1meta~11.json",
+    "$schema": "http://json-schema.org/draft-04/schema#",
     "type": "string",
     "definitions": {
       "https://example.com/meta/1.json": {
-        "$schema": "#/definitions/https:~1~1example.com~1meta~12.json"
+        "$schema": "http://json-schema.org/draft-04/schema#"
       },
       "https://example.com/meta/2.json": {
         "$schema": "http://json-schema.org/draft-04/schema#"
