ef213f6089fe3892b8de84b1a5b8a608c071ed6c
Juan Cruz Viotti <jv@jviotti.com>
2025-08-26 13:11:07 -0400

    Attempt to remove dynamic references in `for_editor` (#1965)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/extension/editorschema/editorschema.cc
+++ src/extension/editorschema/editorschema.cc
@@ -2,6 +2,53 @@
 
 #include <cassert> // assert
 
+namespace {
+
+// Note that we don't take into account dynamic resources behind conditionals,
+// etc. We probably should, but the complexity of this transformation would
+// massively grow, plus such case is quite uncommon in practice.
+// See https://arxiv.org/abs/2503.11288 for an academic study of this topic
+auto top_dynamic_anchor_location(
+    const sourcemeta::core::SchemaFrame &frame,
+    const sourcemeta::core::Pointer &current,
+    const sourcemeta::core::JSON::String &fragment,
+    const sourcemeta::core::JSON::String &default_uri)
+    -> std::optional<sourcemeta::core::Pointer> {
+  // Get the location object of where we are at the moment
+  const auto uri{frame.uri(current)};
+  assert(uri.has_value());
+  const auto match{frame.traverse(uri.value().get())};
+  assert(match.has_value());
+  const auto &location{match.value().get()};
+
+  // Try to locate an anchor with the given name on the current base
+  assert(!fragment.starts_with('#'));
+  const auto anchor_uri{location.base + "#" + fragment};
+  const auto anchor{frame.traverse(anchor_uri)};
+
+  if (location.parent.has_value()) {
+    // If there is a parent resource, keep looking there, but update the default
+    // if the current resource has the dynamic anchor we want
+    return top_dynamic_anchor_location(frame, location.parent.value(), fragment,
+                                       anchor.has_value() ? anchor_uri
+                                                          : default_uri);
+
+    // If we are at the top of the schema and it declares the dynamic anchor, we
+    // should use that
+  } else if (anchor.has_value()) {
+    return anchor.value().get().pointer;
+
+    // Otherwise, if we are at the top and the dynamic anchor is not there, use
+    // the default we have so far
+  } else {
+    const auto default_location{frame.traverse(default_uri)};
+    assert(default_location.has_value());
+    return default_location.value().get().pointer;
+  }
+}
+
+} // namespace
+
 namespace sourcemeta::core {
 
 auto for_editor(JSON &schema, const SchemaWalker &walker,
@@ -43,44 +90,56 @@ auto for_editor(JSON &schema, const SchemaWalker &walker,
     }
   }
 
-  // (4) Fix-up static references based on pointers from the root
+  // (4) Fix-up static and dynamic references
   for (const auto &[key, reference] : frame.references()) {
-    if (key.first != SchemaReferenceType::Static) {
-      continue;
-    }
-
     assert(!key.second.empty());
     assert(key.second.back().is_property());
     const auto &keyword{key.second.back().to_property()};
 
-    // The `$schema` keyword is not allowed to take relative URIs (for example,
-    // pointers going from the root). Because we remove identifiers, the only
-    // sane thing we can do here is default it to the base dialect, which
-    // editors will likely understand
-    if (keyword == "$schema") {
-      const auto uri{frame.uri(key.second)};
-      assert(uri.has_value());
-      const auto origin{frame.traverse(uri.value().get())};
-      assert(origin.has_value());
-      set(schema, key.second, JSON{origin.value().get().base_dialect});
-      continue;
-    }
+    if (key.first == SchemaReferenceType::Dynamic) {
+      if (reference.fragment.has_value()) {
+        auto destination{top_dynamic_anchor_location(frame, key.second,
+                                                     reference.fragment.value(),
+                                                     reference.destination)};
+        if (!destination.has_value()) {
+          continue;
+        }
 
-    // As we get rid of identifiers, we rephrase every reference to be the URI
-    // representation of the JSON Pointer to the destination from the root
-    const auto result{frame.traverse(reference.destination)};
-    if (result.has_value()) {
-      set(schema, key.second,
-          JSON{to_uri(result.value().get().pointer).recompose()});
-
-      // If we have a dynamic reference to a static location,
-      // we can just rename the keyword
-      if (keyword == "$dynamicRef" || keyword == "$recursiveRef") {
-        get(schema, key.second.initial()).rename(keyword, "$ref");
+        set(schema, key.second,
+            JSON{to_uri(std::move(destination).value()).recompose()});
       }
 
+      get(schema, key.second.initial()).rename(keyword, "$ref");
     } else {
-      set(schema, key.second, JSON{reference.destination});
+      // The `$schema` keyword is not allowed to take relative URIs (for
+      // example, pointers going from the root). Because we remove identifiers,
+      // the only sane thing we can do here is default it to the base dialect,
+      // which editors will likely understand
+      if (keyword == "$schema") {
+        const auto uri{frame.uri(key.second)};
+        assert(uri.has_value());
+        const auto origin{frame.traverse(uri.value().get())};
+        assert(origin.has_value());
+        set(schema, key.second, JSON{origin.value().get().base_dialect});
+        continue;
+      }
+
+      // As we get rid of identifiers, we rephrase every reference to be the URI
+      // representation of the JSON Pointer to the destination from the root
+      const auto result{frame.traverse(reference.destination)};
+      if (result.has_value()) {
+        set(schema, key.second,
+            JSON{to_uri(result.value().get().pointer).recompose()});
+
+        // If we have a dynamic reference to a static location,
+        // we can just rename the keyword
+        if (keyword == "$dynamicRef" || keyword == "$recursiveRef") {
+          get(schema, key.second.initial()).rename(keyword, "$ref");
+        }
+
+      } else {
+        set(schema, key.second, JSON{reference.destination});
+      }
     }
   }
 }
