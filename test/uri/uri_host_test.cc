49bdeed335497da3672c0e1a1b42c82668ad68a3
Juan Cruz Viotti <jv@jviotti.com>
2025-10-13 09:02:53 -0400

    Extend URI testing for extra compliance (#2003)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/uri/uri_host_test.cc
+++ test/uri/uri_host_test.cc
@@ -29,3 +29,77 @@ TEST(URI_host, urn) {
   const sourcemeta::core::URI uri{"urn:example:schema"};
   EXPECT_FALSE(uri.host().has_value());
 }
+
+TEST(URI_host, rfc3986_ipv4_address) {
+  const sourcemeta::core::URI uri{"http://192.168.1.1/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "192.168.1.1");
+}
+
+TEST(URI_host, rfc3986_ipv4_address_with_port) {
+  const sourcemeta::core::URI uri{"http://192.168.1.1:8080/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "192.168.1.1");
+}
+
+TEST(URI_host, rfc3986_ipv6_address) {
+  const sourcemeta::core::URI uri{"http://[2001:db8::1]/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "2001:db8::1");
+}
+
+TEST(URI_host, rfc3986_ipv6_address_with_port) {
+  const sourcemeta::core::URI uri{"http://[2001:db8::1]:8080/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "2001:db8::1");
+}
+
+TEST(URI_host, rfc3986_ipv6_localhost) {
+  const sourcemeta::core::URI uri{"http://[::1]/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "::1");
+}
+
+TEST(URI_host, rfc3986_host_case_insensitive) {
+  const sourcemeta::core::URI uri1{"http://EXAMPLE.COM/path"};
+  const sourcemeta::core::URI uri2{"http://example.com/path"};
+  EXPECT_EQ(uri1.host().value(), "example.com");
+  EXPECT_EQ(uri2.host().value(), "example.com");
+  EXPECT_EQ(uri1.host().value(), uri2.host().value());
+}
+
+TEST(URI_host, rfc3986_host_with_hyphen) {
+  const sourcemeta::core::URI uri{"http://my-example-host.com/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "my-example-host.com");
+}
+
+TEST(URI_host, rfc3986_host_with_numbers) {
+  const sourcemeta::core::URI uri{"http://example123.com/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "example123.com");
+}
+
+TEST(URI_host, rfc3986_subdomain) {
+  const sourcemeta::core::URI uri{"http://www.sub.example.com/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "www.sub.example.com");
+}
+
+TEST(URI_host, rfc3986_localhost) {
+  const sourcemeta::core::URI uri{"http://localhost/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "localhost");
+}
+
+TEST(URI_host, rfc3986_empty_host_with_authority) {
+  const sourcemeta::core::URI uri{"file:///path/to/file"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "");
+}
+
+TEST(URI_host, rfc3986_percent_encoded_host) {
+  const sourcemeta::core::URI uri{"http://example%2Ecom/path"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "example.com");
+}
--- test/uri/uri_user_info_test.cc
+++ test/uri/uri_user_info_test.cc
@@ -20,3 +20,50 @@ TEST(URI_user_info, empty_example) {
   sourcemeta::core::URI uri{"http://host:80/"};
   EXPECT_FALSE(uri.userinfo().has_value());
 }
+
+TEST(URI_user_info, rfc3986_username_only) {
+  sourcemeta::core::URI uri{"http://user@host/path"};
+  EXPECT_TRUE(uri.userinfo().has_value());
+  EXPECT_EQ(uri.userinfo().value(), "user");
+}
+
+TEST(URI_user_info, rfc3986_username_empty_password) {
+  sourcemeta::core::URI uri{"http://user:@host/path"};
+  EXPECT_TRUE(uri.userinfo().has_value());
+  EXPECT_EQ(uri.userinfo().value(), "user:");
+}
+
+TEST(URI_user_info, rfc3986_username_password) {
+  sourcemeta::core::URI uri{"http://user:secret@host/path"};
+  EXPECT_TRUE(uri.userinfo().has_value());
+  EXPECT_EQ(uri.userinfo().value(), "user:secret");
+}
+
+TEST(URI_user_info, rfc3986_percent_encoded_userinfo) {
+  sourcemeta::core::URI uri{"http://user%20name:pass%20word@host/path"};
+  EXPECT_TRUE(uri.userinfo().has_value());
+  EXPECT_EQ(uri.userinfo().value(), "user%20name:pass%20word");
+}
+
+TEST(URI_user_info, rfc3986_userinfo_with_subdelims) {
+  sourcemeta::core::URI uri{"http://user!$&'()*+,;=:pass@host/path"};
+  EXPECT_TRUE(uri.userinfo().has_value());
+  EXPECT_EQ(uri.userinfo().value(), "user!$&'()*+,;=:pass");
+}
+
+TEST(URI_user_info, rfc3986_empty_userinfo_marker) {
+  sourcemeta::core::URI uri{"http://@host/path"};
+  EXPECT_TRUE(uri.userinfo().has_value());
+  EXPECT_EQ(uri.userinfo().value(), "");
+}
+
+TEST(URI_user_info, rfc3986_no_authority) {
+  sourcemeta::core::URI uri{"urn:example:resource"};
+  EXPECT_FALSE(uri.userinfo().has_value());
+}
+
+TEST(URI_user_info, rfc3986_userinfo_with_at_sign) {
+  sourcemeta::core::URI uri{"http://user%40domain:pass@host/path"};
+  EXPECT_TRUE(uri.userinfo().has_value());
+  EXPECT_EQ(uri.userinfo().value(), "user%40domain:pass");
+}
