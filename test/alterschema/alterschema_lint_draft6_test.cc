38d686c3e828da4748d4b58bb522bf91e5a0e8b5
Juan Cruz Viotti <jv@jviotti.com>
2025-11-14 16:34:31 -0400

    Don't break semantics in `required_properties_in_properties` (#2050)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/alterschema/alterschema_lint_draft6_test.cc
+++ test/alterschema/alterschema_lint_draft6_test.cc
@@ -2370,6 +2370,48 @@ TEST(AlterSchema_lint_draft6, required_properties_in_properties_5) {
   EXPECT_EQ(document, expected);
 }
 
+TEST(AlterSchema_lint_draft6, required_properties_in_properties_16) {
+  sourcemeta::core::JSON document = sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "required": [ "foo", "bar" ],
+    "additionalProperties": false
+  })JSON");
+
+  LINT_AND_FIX_FOR_READABILITY_STRICT(document);
+
+  const sourcemeta::core::JSON expected = sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "required": [ "foo", "bar" ],
+    "additionalProperties": false
+  })JSON");
+
+  EXPECT_EQ(document, expected);
+}
+
+TEST(AlterSchema_lint_draft6, required_properties_in_properties_17) {
+  sourcemeta::core::JSON document = sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "required": [ "foo", "bar" ],
+    "additionalProperties": false,
+    "properties": {
+      "foo": true
+    }
+  })JSON");
+
+  LINT_AND_FIX_FOR_READABILITY_STRICT(document);
+
+  const sourcemeta::core::JSON expected = sourcemeta::core::parse_json(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "required": [ "foo", "bar" ],
+    "additionalProperties": false,
+    "properties": {
+      "foo": true
+    }
+  })JSON");
+
+  EXPECT_EQ(document, expected);
+}
+
 TEST(AlterSchema_lint_draft6, unknown_keywords_prefix_1) {
   sourcemeta::core::JSON document = sourcemeta::core::parse_json(R"JSON({
     "$schema": "http://json-schema.org/draft-06/schema#",
