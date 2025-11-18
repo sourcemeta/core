a554b188ac86706cd9ba3dcf8cf4b267f86e2043
Juan Cruz Viotti <jv@jviotti.com>
2025-11-18 10:03:37 -0400

    Lint local references to unknown locations (#2038)
    See: https://github.com/sourcemeta/core/issues/1975
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/alterschema/alterschema_lint_draft3_test.cc
+++ test/alterschema/alterschema_lint_draft3_test.cc
@@ -1015,8 +1015,7 @@ TEST(AlterSchema_lint_draft3, draft_ref_siblings_1) {
   LINT_AND_FIX_FOR_READABILITY(document);
 
   const sourcemeta::core::JSON expected = sourcemeta::core::parse_json(R"JSON({
-    "$schema": "http://json-schema.org/draft-03/schema#",
-    "$ref": "#/definitions/foo"
+    "$schema": "http://json-schema.org/draft-03/schema#"
   })JSON");
 
   EXPECT_EQ(document, expected);
@@ -1035,7 +1034,6 @@ TEST(AlterSchema_lint_draft3, draft_ref_siblings_2) {
 
   const sourcemeta::core::JSON expected = sourcemeta::core::parse_json(R"JSON({
     "$schema": "http://json-schema.org/draft-03/schema#",
-    "$ref": "#/definitions/foo",
     "description": "A string field"
   })JSON");
 
@@ -1058,7 +1056,6 @@ TEST(AlterSchema_lint_draft3, draft_ref_siblings_3) {
 
   const sourcemeta::core::JSON expected = sourcemeta::core::parse_json(R"JSON({
     "$schema": "http://json-schema.org/draft-03/schema#",
-    "$ref": "#/definitions/foo",
     "title": "Test Schema",
     "default": null
   })JSON");
@@ -1079,7 +1076,6 @@ TEST(AlterSchema_lint_draft3, draft_ref_siblings_4) {
 
   const sourcemeta::core::JSON expected = sourcemeta::core::parse_json(R"JSON({
     "$schema": "http://json-schema.org/draft-03/schema#",
-    "$ref": "#/definitions/foo",
     "description": "Documentation"
   })JSON");
 
@@ -1307,8 +1303,7 @@ TEST(AlterSchema_lint_draft3, unknown_keywords_prefix_10) {
   LINT_AND_FIX_FOR_READABILITY(document);
 
   const sourcemeta::core::JSON expected = sourcemeta::core::parse_json(R"JSON({
-    "$schema": "http://json-schema.org/draft-03/schema#",
-    "$ref": "#/definitions/MyType"
+    "$schema": "http://json-schema.org/draft-03/schema#"
   })JSON");
 
   EXPECT_EQ(document, expected);
