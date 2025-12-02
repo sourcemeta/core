10258d67f6bdfc492583151f42de6da4c0e3e046
Juan Cruz Viotti <jv@jviotti.com>
2025-12-02 14:50:58 -0400

    [WIP] Implement the RFC 3492 Punycode encoding
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/core/punycode/include/sourcemeta/core/punycode_error.h
@@ -0,0 +1,40 @@
+#ifndef SOURCEMETA_CORE_PUNYCODE_ERROR_H_
+#define SOURCEMETA_CORE_PUNYCODE_ERROR_H_
+
+#ifndef SOURCEMETA_CORE_PUNYCODE_EXPORT
+#include <sourcemeta/core/punycode_error.h>
+#endif
+
+#include <exception> // std::exception
+#include <string>    // std::string
+#include <utility>   // std::move
+
+namespace sourcemeta::core {
+
+// Exporting symbols that depends on the standard C++ library is considered
+// safe.
+// https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275?view=msvc-170&redirectedfrom=MSDN
+#if defined(_MSC_VER)
+#pragma warning(disable : 4251 4275)
+#endif
+
+/// @ingroup punycode
+class SOURCEMETA_CORE_PUNYCODE_EXPORT PunycodeError : public std::exception {
+public:
+  PunycodeError(std::string message) : message_{std::move(message)} {}
+
+  [[nodiscard]] auto what() const noexcept -> const char * override {
+    return this->message_.c_str();
+  }
+
+private:
+  std::string message_;
+};
+
+#if defined(_MSC_VER)
+#pragma warning(default : 4251 4275)
+#endif
+
+} // namespace sourcemeta::core
+
+#endif
