ebe5d5498c997331859fa3a8d8f9d9173c8f46bd
Juan Cruz Viotti <jv@jviotti.com>
2025-12-06 11:18:28 -0400

    Benchmark converting frames and trackers to JSON (#2096)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/jsonschema.cc
+++ benchmark/jsonschema.cc
@@ -62,6 +62,52 @@ static void Schema_Frame_ISO_Country_Locations(benchmark::State &state) {
   }
 }
 
+static void
+Schema_Frame_ISO_Country_Locations_To_JSON(benchmark::State &state) {
+  sourcemeta::core::PointerPositionTracker tracker;
+  const auto schema{sourcemeta::core::read_json(
+      std::filesystem::path{CURRENT_DIRECTORY} / "schemas" /
+          "2020_12_iso_country_2023_set_3.json",
+      std::ref(tracker))};
+
+  sourcemeta::core::SchemaFrame frame{
+      sourcemeta::core::SchemaFrame::Mode::Locations};
+  frame.analyse(schema, sourcemeta::core::schema_official_walker,
+                sourcemeta::core::schema_official_resolver);
+
+  for (auto _ : state) {
+    auto result{frame.to_json(tracker)};
+    assert(result.is_object());
+    benchmark::DoNotOptimize(result);
+  }
+}
+
+static void Schema_Tracker_ISO_Country(benchmark::State &state) {
+  for (auto _ : state) {
+    sourcemeta::core::PointerPositionTracker tracker;
+    auto schema{sourcemeta::core::read_json(
+        std::filesystem::path{CURRENT_DIRECTORY} / "schemas" /
+            "2020_12_iso_country_2023_set_3.json",
+        std::ref(tracker))};
+    assert(schema.is_object());
+    benchmark::DoNotOptimize(schema);
+  }
+}
+
+static void Schema_Tracker_ISO_Country_To_JSON(benchmark::State &state) {
+  sourcemeta::core::PointerPositionTracker tracker;
+  sourcemeta::core::read_json(std::filesystem::path{CURRENT_DIRECTORY} /
+                                  "schemas" /
+                                  "2020_12_iso_country_2023_set_3.json",
+                              std::ref(tracker));
+
+  for (auto _ : state) {
+    auto result{sourcemeta::core::to_json(tracker)};
+    assert(result.is_object());
+    benchmark::DoNotOptimize(result);
+  }
+}
+
 static void Schema_Bundle_Meta_2020_12(benchmark::State &state) {
   for (auto _ : state) {
     state.PauseTiming();
@@ -79,4 +125,7 @@ BENCHMARK(Schema_Frame_OMC_Instances);
 BENCHMARK(Schema_Frame_OMC_References);
 BENCHMARK(Schema_Frame_OMC_Locations);
 BENCHMARK(Schema_Frame_ISO_Country_Locations);
+BENCHMARK(Schema_Frame_ISO_Country_Locations_To_JSON);
+BENCHMARK(Schema_Tracker_ISO_Country);
+BENCHMARK(Schema_Tracker_ISO_Country_To_JSON);
 BENCHMARK(Schema_Bundle_Meta_2020_12);
