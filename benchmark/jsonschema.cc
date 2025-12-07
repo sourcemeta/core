1de44ea8c349f9db59c92cf261b53260ac28afdf
Juan Cruz Viotti <jv@jviotti.com>
2025-12-07 08:49:21 -0400

    Update benchmark test schema to the ISO country one (#2099)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/jsonschema.cc
+++ benchmark/jsonschema.cc
@@ -48,10 +48,10 @@ static void Schema_Frame_OMC_Locations(benchmark::State &state) {
   }
 }
 
-static void Schema_Frame_ISO_Country_Locations(benchmark::State &state) {
+static void Schema_Frame_ISO_Language_Locations(benchmark::State &state) {
   const auto schema{sourcemeta::core::read_json(
       std::filesystem::path{CURRENT_DIRECTORY} / "schemas" /
-      "2020_12_iso_country_2023_set_3.json")};
+      "2020_12_iso_language_2023_set_3.json")};
 
   for (auto _ : state) {
     sourcemeta::core::SchemaFrame frame{
@@ -63,11 +63,11 @@ static void Schema_Frame_ISO_Country_Locations(benchmark::State &state) {
 }
 
 static void
-Schema_Frame_ISO_Country_Locations_To_JSON(benchmark::State &state) {
+Schema_Frame_ISO_Language_Locations_To_JSON(benchmark::State &state) {
   sourcemeta::core::PointerPositionTracker tracker;
   const auto schema{sourcemeta::core::read_json(
       std::filesystem::path{CURRENT_DIRECTORY} / "schemas" /
-          "2020_12_iso_country_2023_set_3.json",
+          "2020_12_iso_language_2023_set_3.json",
       std::ref(tracker))};
 
   sourcemeta::core::SchemaFrame frame{
@@ -82,23 +82,23 @@ Schema_Frame_ISO_Country_Locations_To_JSON(benchmark::State &state) {
   }
 }
 
-static void Schema_Tracker_ISO_Country(benchmark::State &state) {
+static void Schema_Tracker_ISO_Language(benchmark::State &state) {
   for (auto _ : state) {
     sourcemeta::core::PointerPositionTracker tracker;
     auto schema{sourcemeta::core::read_json(
         std::filesystem::path{CURRENT_DIRECTORY} / "schemas" /
-            "2020_12_iso_country_2023_set_3.json",
+            "2020_12_iso_language_2023_set_3.json",
         std::ref(tracker))};
     assert(schema.is_object());
     benchmark::DoNotOptimize(schema);
   }
 }
 
-static void Schema_Tracker_ISO_Country_To_JSON(benchmark::State &state) {
+static void Schema_Tracker_ISO_Language_To_JSON(benchmark::State &state) {
   sourcemeta::core::PointerPositionTracker tracker;
   sourcemeta::core::read_json(std::filesystem::path{CURRENT_DIRECTORY} /
                                   "schemas" /
-                                  "2020_12_iso_country_2023_set_3.json",
+                                  "2020_12_iso_language_2023_set_3.json",
                               std::ref(tracker));
 
   for (auto _ : state) {
@@ -108,12 +108,12 @@ static void Schema_Tracker_ISO_Country_To_JSON(benchmark::State &state) {
   }
 }
 
-static void Schema_Format_ISO_Country_To_JSON(benchmark::State &state) {
+static void Schema_Format_ISO_Language_To_JSON(benchmark::State &state) {
   for (auto _ : state) {
     state.PauseTiming();
     auto schema{sourcemeta::core::read_json(
         std::filesystem::path{CURRENT_DIRECTORY} / "schemas" /
-        "2020_12_iso_country_2023_set_3.json")};
+        "2020_12_iso_language_2023_set_3.json")};
     state.ResumeTiming();
     sourcemeta::core::format(schema, sourcemeta::core::schema_official_walker,
                              sourcemeta::core::schema_official_resolver);
@@ -137,9 +137,9 @@ static void Schema_Bundle_Meta_2020_12(benchmark::State &state) {
 BENCHMARK(Schema_Frame_OMC_Instances);
 BENCHMARK(Schema_Frame_OMC_References);
 BENCHMARK(Schema_Frame_OMC_Locations);
-BENCHMARK(Schema_Frame_ISO_Country_Locations);
-BENCHMARK(Schema_Frame_ISO_Country_Locations_To_JSON);
-BENCHMARK(Schema_Tracker_ISO_Country);
-BENCHMARK(Schema_Tracker_ISO_Country_To_JSON);
-BENCHMARK(Schema_Format_ISO_Country_To_JSON);
+BENCHMARK(Schema_Frame_ISO_Language_Locations);
+BENCHMARK(Schema_Frame_ISO_Language_Locations_To_JSON);
+BENCHMARK(Schema_Tracker_ISO_Language);
+BENCHMARK(Schema_Tracker_ISO_Language_To_JSON);
+BENCHMARK(Schema_Format_ISO_Language_To_JSON);
 BENCHMARK(Schema_Bundle_Meta_2020_12);
