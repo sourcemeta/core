ef1081e1b9bd4f47eb92c5f71cac07281e0fe28a
Juan Cruz Viotti <jv@jviotti.com>
2025-12-23 11:39:18 -0400

    Add a canonicalize CLI for testing purposes (#2138)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/alterschema/alterschema_canonicalize.cc
@@ -0,0 +1,35 @@
+#include <sourcemeta/core/alterschema.h>
+#include <sourcemeta/core/json.h>
+#include <sourcemeta/core/jsonschema.h>
+
+#include <cstdlib>  // EXIT_SUCCESS, EXIT_FAILURE
+#include <iostream> // std::cerr, std::cout
+
+auto main(int argc, char *argv[]) -> int {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <schema.json>\n";
+    return EXIT_FAILURE;
+  }
+
+  auto document{sourcemeta::core::read_json(argv[1])};
+
+  sourcemeta::core::SchemaTransformer bundle;
+  sourcemeta::core::add(bundle,
+                        sourcemeta::core::AlterSchemaMode::Canonicalizer);
+  const auto result{bundle.apply(document, sourcemeta::core::schema_walker,
+                                 sourcemeta::core::schema_resolver,
+                                 [](const auto &pointer, const auto &name,
+                                    const auto &message, const auto &) {
+                                   std::cerr
+                                       << sourcemeta::core::to_string(pointer)
+                                       << ": " << name << ": " << message
+                                       << "\n";
+                                 })};
+
+  sourcemeta::core::format(document, sourcemeta::core::schema_walker,
+                           sourcemeta::core::schema_resolver);
+  sourcemeta::core::prettify(document, std::cout);
+  std::cout << "\n";
+
+  return result.first ? EXIT_SUCCESS : EXIT_FAILURE;
+}
