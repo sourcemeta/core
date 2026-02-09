df5f2861e1e6e8c1ba5879f4069949fbdd0f924a
Juan Cruz Viotti <jv@jviotti.com>
2026-02-09 08:27:08 -0400

    Rename MD5 module to Crypto
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ src/core/crypto/include/sourcemeta/core/crypto.h
@@ -0,0 +1,15 @@
+#ifndef SOURCEMETA_CORE_CRYPTO_H_
+#define SOURCEMETA_CORE_CRYPTO_H_
+
+/// @defgroup crypto Crypto
+/// @brief Cryptographic hash functions.
+///
+/// This functionality is included as follows:
+///
+/// ```cpp
+/// #include <sourcemeta/core/crypto.h>
+/// ```
+
+#include <sourcemeta/core/crypto_md5.h>
+
+#endif
