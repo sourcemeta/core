a672e22956749ffe435104ed1e546e287ff8ddcd
Juan Cruz Viotti <jv@jviotti.com>
2026-02-18 10:15:38 -0400

    Fix various URI parsing spec deviations (#2254)
    Fixes: https://github.com/sourcemeta/core/issues/2253
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/uri/uri_parse_test.cc
+++ test/uri/uri_parse_test.cc
@@ -100,6 +100,87 @@ TEST(URI_parse, syntax_error_percent_in_host) {
                sourcemeta::core::URIParseError);
 }
 
+// RFC 3986: relative-ref path-noscheme means the first segment of a
+// schemeless, authorityless path must not contain a colon
+TEST(URI_parse, syntax_error_digit_prefix_scheme_like) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"2http://example.com"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, syntax_error_underscore_scheme_like) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"my_scheme://example.com"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, syntax_error_colon_slash_slash) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"://example.com"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, syntax_error_hyphen_prefix_scheme_like) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"-http://example.com"},
+               sourcemeta::core::URIParseError);
+}
+
+// RFC 3986: path-abempty after authority must start with "/" or be empty
+TEST(URI_parse, syntax_error_port_trailing_alpha) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"http://example.com:80a"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, syntax_error_port_trailing_range) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"http://example.com:80-90"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, syntax_error_bare_ipv6_no_brackets) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"http://2001:db8::1"},
+               sourcemeta::core::URIParseError);
+}
+
+// RFC 3986: IPv6 brackets must contain valid hex digits, colons, and dots
+TEST(URI_parse, syntax_error_ipv6_invalid_hex) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"http://[2001:db8::gggg]"},
+               sourcemeta::core::URIParseError);
+}
+
+// RFC 3986: IPvFuture = "v" 1*HEXDIG "." 1*( unreserved / sub-delims / ":" )
+// Must have version hex digits and dot separator
+TEST(URI_parse, syntax_error_ipvfuture_bare_v) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"http://[v]"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, syntax_error_ipvfuture_missing_dot) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"http://[vabc]"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, syntax_error_ipvfuture_non_hex_version) {
+  EXPECT_THROW(sourcemeta::core::URI uri{"http://[vZ.foo]"},
+               sourcemeta::core::URIParseError);
+}
+
+TEST(URI_parse, success_ipvfuture_valid) {
+  sourcemeta::core::URI uri{"http://[vFF.a:b]"};
+  EXPECT_EQ(uri.host().value(), "vFF.a:b");
+  EXPECT_EQ(uri.recompose(), "http://[vFF.a:b]");
+}
+
+// RFC 3986: port = *DIGIT (empty port after colon is valid)
+TEST(URI_parse, rfc3986_empty_port) {
+  sourcemeta::core::URI uri{"http://example.com:"};
+  EXPECT_EQ(uri.host().value(), "example.com");
+  EXPECT_FALSE(uri.port().has_value());
+}
+
+TEST(URI_parse, rfc3986_empty_port_with_path) {
+  sourcemeta::core::URI uri{"http://example.com:/path"};
+  EXPECT_EQ(uri.host().value(), "example.com");
+  EXPECT_FALSE(uri.port().has_value());
+  EXPECT_EQ(uri.path().value(), "/path");
+}
+
 // Inspired from
 // https://github.com/uriparser/uriparser/blob/bf0174e83164a4659c51c135399478bec389eafa/test/test.cpp#L315
 
