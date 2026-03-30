b3a3d5faba9e4d50ae98b999f8ea146a3ab9a57a
Juan Cruz Viotti <jv@jviotti.com>
2026-03-30 10:32:40 -0400

    Fix `URI::is_relative` incorrect behaviour (#2323)
    Fixes: https://github.com/sourcemeta/core/issues/2318
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/uri/uri_is_relative_test.cc
+++ test/uri/uri_is_relative_test.cc
@@ -22,3 +22,23 @@ TEST(URI_is_relative, unique_slash) {
   const sourcemeta::core::URI uri{"/foo"};
   EXPECT_TRUE(uri.is_relative());
 }
+
+// RFC 3986 Section 4: a URI with a scheme is never a relative reference,
+// regardless of path content
+TEST(URI_is_relative, scheme_with_dot_path_is_not_relative) {
+  const sourcemeta::core::URI uri{"a:./foo"};
+  EXPECT_TRUE(uri.is_absolute());
+  EXPECT_FALSE(uri.is_relative());
+}
+
+TEST(URI_is_relative, scheme_with_dotdot_path_is_not_relative) {
+  const sourcemeta::core::URI uri{"a:../foo"};
+  EXPECT_TRUE(uri.is_absolute());
+  EXPECT_FALSE(uri.is_relative());
+}
+
+TEST(URI_is_relative, scheme_with_dot_only_is_not_relative) {
+  const sourcemeta::core::URI uri{"a:."};
+  EXPECT_TRUE(uri.is_absolute());
+  EXPECT_FALSE(uri.is_relative());
+}
