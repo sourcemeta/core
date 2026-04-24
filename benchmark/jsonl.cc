a9574d007d92a184d6ed7f67ffa57c00a4549d43
Juan Cruz Viotti <jv@jviotti.com>
2026-04-24 14:49:27 -0400

    Add a JSONL benchmark case (#2358)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ benchmark/jsonl.cc
@@ -0,0 +1,28 @@
+#include <benchmark/benchmark.h>
+
+#include <sourcemeta/core/jsonl.h>
+
+#include <cassert>    // assert
+#include <cstddef>    // std::size_t
+#include <filesystem> // std::filesystem
+#include <fstream>    // std::ifstream
+#include <string>     // std::string
+
+static void JSONL_Parse_Large(benchmark::State &state) {
+  const std::filesystem::path filepath{std::string{CURRENT_DIRECTORY} +
+                                       "/files/large.jsonl"};
+
+  for (auto _ : state) {
+    std::ifstream stream{filepath};
+    assert(stream.is_open());
+    std::size_t count{0};
+    for ([[maybe_unused]] const auto &entry : sourcemeta::core::JSONL{stream}) {
+      count += 1;
+    }
+
+    assert(count == 10000);
+    benchmark::DoNotOptimize(count);
+  }
+}
+
+BENCHMARK(JSONL_Parse_Large);
