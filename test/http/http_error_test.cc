eb361a9f3a01f7cffb5da7946a9516ee11dc9ede
Juan Cruz Viotti <jv@jviotti.com>
2026-06-12 11:46:32 -0400

    Add a new `HTTPStatusError` exception class (#2492)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/http/http_error_test.cc
+++ test/http/http_error_test.cc
@@ -2,8 +2,11 @@
 
 #include <sourcemeta/core/http_error.h>
 #include <sourcemeta/core/http_method.h>
+#include <sourcemeta/core/http_status.h>
 
+#include <optional>  // std::optional
 #include <stdexcept> // std::runtime_error
+#include <string>    // std::string
 
 TEST(HTTP_error, message) {
   const sourcemeta::core::HTTPError error{sourcemeta::core::HTTPMethod::GET,
@@ -31,4 +34,56 @@ TEST(HTTP_error, catchable_as_runtime_error) {
       throw sourcemeta::core::HTTPError(sourcemeta::core::HTTPMethod::GET,
                                         "https://example.com", "failure"),
       std::runtime_error);
+}
+
+TEST(HTTP_error, status_error_message) {
+  const sourcemeta::core::HTTPStatusError error{
+      sourcemeta::core::HTTPMethod::GET, "https://example.com",
+      sourcemeta::core::HTTP_STATUS_NOT_FOUND};
+  EXPECT_STREQ(error.what(), "Unsuccessful HTTP response");
+}
+
+TEST(HTTP_error, status_error_method) {
+  const sourcemeta::core::HTTPStatusError error{
+      sourcemeta::core::HTTPMethod::POST, "https://example.com",
+      sourcemeta::core::HTTP_STATUS_NOT_FOUND};
+  EXPECT_EQ(error.method(), sourcemeta::core::HTTPMethod::POST);
+}
+
+TEST(HTTP_error, status_error_url) {
+  const sourcemeta::core::HTTPStatusError error{
+      sourcemeta::core::HTTPMethod::GET, "https://example.com/schema.json",
+      sourcemeta::core::HTTP_STATUS_NOT_FOUND};
+  EXPECT_EQ(error.url(), "https://example.com/schema.json");
+}
+
+TEST(HTTP_error, status_error_status) {
+  const sourcemeta::core::HTTPStatusError error{
+      sourcemeta::core::HTTPMethod::GET, "https://example.com",
+      sourcemeta::core::HTTP_STATUS_SERVICE_UNAVAILABLE};
+  EXPECT_EQ(error.status(), sourcemeta::core::HTTP_STATUS_SERVICE_UNAVAILABLE);
+  EXPECT_EQ(error.status().code, 503);
+}
+
+TEST(HTTP_error, status_error_catchable_as_http_error) {
+  EXPECT_THROW(throw sourcemeta::core::HTTPStatusError(
+                   sourcemeta::core::HTTPMethod::GET, "https://example.com",
+                   sourcemeta::core::HTTP_STATUS_NOT_FOUND),
+               sourcemeta::core::HTTPError);
+}
+
+TEST(HTTP_error, status_error_owns_status_data) {
+  std::optional<sourcemeta::core::HTTPStatusError> error;
+  {
+    const std::string phrase{"Custom Experimental Phrase"};
+    const std::string wire{"599 Custom Experimental Phrase"};
+    const sourcemeta::core::HTTPStatus status{
+        .code = 599, .phrase = phrase, .wire = wire};
+    error.emplace(sourcemeta::core::HTTPMethod::GET, "https://example.com",
+                  status);
+  }
+
+  EXPECT_EQ(error.value().status().code, 599);
+  EXPECT_EQ(error.value().status().phrase, "Custom Experimental Phrase");
+  EXPECT_EQ(error.value().status().wire, "599 Custom Experimental Phrase");
 }
