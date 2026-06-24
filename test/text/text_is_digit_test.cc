62be14457034acc6ffb91f46251c2c6553bc70df
Juan Cruz Viotti <jv@jviotti.com>
2026-06-23 21:08:49 -0400

    Add some more text utilities
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/text/text_is_digit_test.cc
+++ test/text/text_is_digit_test.cc
@@ -23,3 +23,15 @@ TEST(Text_is_digit, ascii_boundaries) {
   EXPECT_FALSE(sourcemeta::core::is_digit('/'));
   EXPECT_FALSE(sourcemeta::core::is_digit(':'));
 }
+
+TEST(Text_is_digit, string_all_digits) {
+  EXPECT_TRUE(sourcemeta::core::is_digit("0123456789"));
+}
+
+TEST(Text_is_digit, string_with_letter) {
+  EXPECT_FALSE(sourcemeta::core::is_digit("12a"));
+}
+
+TEST(Text_is_digit, empty_string) {
+  EXPECT_FALSE(sourcemeta::core::is_digit(""));
+}
