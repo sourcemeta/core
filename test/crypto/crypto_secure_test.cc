23ae34eaa9c1a4c71cd19d5f138dfe97d0141536
Juan Cruz Viotti <jv@jviotti.com>
2026-07-19 15:05:12 -0300

    Tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/crypto/crypto_secure_test.cc
+++ test/crypto/crypto_secure_test.cc
@@ -29,3 +29,23 @@ TEST(secure_scope_clears_on_exit) {
 
   EXPECT_EQ(secret, std::string(8, '\x00'));
 }
+
+TEST(secure_string_holds_its_content) {
+  const sourcemeta::core::SecureString secret{"hunter2"};
+  EXPECT_TRUE(secret == "hunter2");
+  EXPECT_EQ(secret.size(), 7);
+}
+
+TEST(secure_string_appends_content) {
+  sourcemeta::core::SecureString secret{"hunter"};
+  secret.append("2");
+  EXPECT_TRUE(secret == "hunter2");
+}
+
+TEST(secure_string_grows_across_a_reallocation) {
+  sourcemeta::core::SecureString secret{"a"};
+  secret.append(1000, 'b');
+  EXPECT_EQ(secret.size(), 1001);
+  EXPECT_EQ(secret.front(), 'a');
+  EXPECT_EQ(secret.back(), 'b');
+}
