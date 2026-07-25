99b6a5e8df44d5cdd78a364eac4a059315884f3d
Juan Cruz Viotti <jv@jviotti.com>
2026-07-25 14:19:38 -0300

    Add a wiping request body path to the HTTP client (#2682)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/crypto/crypto_secure_test.cc
+++ test/crypto/crypto_secure_test.cc
@@ -36,6 +36,18 @@ TEST(secure_string_holds_its_content) {
   EXPECT_EQ(secret.size(), 7);
 }
 
+TEST(secure_string_exposes_its_bytes_through_data) {
+  const sourcemeta::core::SecureString secret{"hunter2"};
+  EXPECT_EQ(std::string_view(secret.data(), secret.size()), "hunter2");
+}
+
+TEST(secure_string_of_empty_content_views_as_empty) {
+  const sourcemeta::core::SecureString secret{};
+  const std::string_view view{secret};
+  EXPECT_TRUE(view.empty());
+  EXPECT_EQ(view, std::string_view{});
+}
+
 TEST(secure_string_appends_content) {
   sourcemeta::core::SecureString secret{"hunter"};
   secret.append("2");
