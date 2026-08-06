0af17b8961004c1908075a8d425fc2826fca13bd
Vansh <officialbusiness9818@gmail.com>
2026-08-06 16:30:28 +0530

    Fix mimalloc target naming, sanitizer ordering, and restore corrupted benchmark CMakeLists
    Signed-off-by: Vansh <officialbusiness9818@gmail.com>

--- cmake/FindMimalloc.cmake
+++ cmake/FindMimalloc.cmake
@@ -1,18 +1,19 @@
-if (NOT Mimalloc_FOUND)
-set(MIMALLOC_DIR "${PROJECT_SOURCE_DIR}/vendor/mimalloc")
+if(NOT Mimalloc_FOUND)
+  set(MIMALLOC_DIR "${PROJECT_SOURCE_DIR}/vendor/mimalloc")
 
-    set(MI_BUILD_SHARED OFF CACHE BOOL
-        "" FORCE) set(MI_BUILD_OBJECT OFF CACHE BOOL
-                      "" FORCE) set(MI_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-        set(MI_OVERRIDE ON CACHE BOOL "" FORCE)
+  set(MI_BUILD_SHARED OFF CACHE BOOL "" FORCE)
+  set(MI_BUILD_OBJECT OFF CACHE BOOL "" FORCE)
+  set(MI_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+  set(MI_OVERRIDE ON CACHE BOOL "" FORCE)
 
-            add_subdirectory(
-                "${MIMALLOC_DIR}"
-                "${CMAKE_CURRENT_BINARY_DIR}/mimalloc" EXCLUDE_FROM_ALL)
+  add_subdirectory(
+    "${MIMALLOC_DIR}"
+    "${CMAKE_CURRENT_BINARY_DIR}/mimalloc" EXCLUDE_FROM_ALL)
 
-                if (TARGET mimalloc - static) set_target_properties(
-                    mimalloc - static PROPERTIES COMPILE_WARNING_AS_ERROR OFF)
-                    add_library(Mimalloc::Mimalloc ALIAS mimalloc - static)
-                        endif()
-
-                            set(Mimalloc_FOUND ON) endif()
+  if(TARGET mimalloc-static)
+    set_target_properties(mimalloc-static
+      PROPERTIES COMPILE_WARNING_AS_ERROR OFF)
+    add_library(Mimalloc::Mimalloc ALIAS mimalloc-static)
+    set(Mimalloc_FOUND ON)
+  endif()
+endif()
