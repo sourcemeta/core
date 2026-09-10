faa2d1a3a5aaff24a786f9e404b274152cfaeb23
Juan Cruz Viotti <jv@jviotti.com>
2026-09-10 13:08:04 -0300

    Migrate Blaze's Foundation module as a `src/core/jsonschema` module here (#2804)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/jsonpointer.cc
+++ benchmark/jsonpointer.cc
@@ -221,9 +221,41 @@ Pointer_Position_Tracker_Get_Deeply_Nested(benchmark::State &state) {
   }
 }
 
+// NOLINTNEXTLINE(readability-identifier-naming)
+static void Schema_Tracker_ISO_Language(benchmark::State &state) {
+  for (auto iteration : state) {
+    sourcemeta::core::PointerPositionTracker tracker;
+    sourcemeta::core::JSON schema{nullptr};
+    sourcemeta::core::read_json(std::filesystem::path{CURRENT_DIRECTORY} /
+                                    "files" /
+                                    "2020_12_iso_language_2023_set_3.json",
+                                schema, std::ref(tracker));
+    assert(schema.is_object());
+    benchmark::DoNotOptimize(schema);
+  }
+}
+
+// NOLINTNEXTLINE(readability-identifier-naming)
+static void Schema_Tracker_ISO_Language_To_JSON(benchmark::State &state) {
+  sourcemeta::core::PointerPositionTracker tracker;
+  sourcemeta::core::JSON schema{nullptr};
+  sourcemeta::core::read_json(std::filesystem::path{CURRENT_DIRECTORY} /
+                                  "files" /
+                                  "2020_12_iso_language_2023_set_3.json",
+                              schema, std::ref(tracker));
+
+  for (auto iteration : state) {
+    auto result{sourcemeta::core::to_json(tracker)};
+    assert(result.is_object());
+    benchmark::DoNotOptimize(result);
+  }
+}
+
 BENCHMARK(Pointer_Object_Traverse);
 BENCHMARK(Pointer_Object_Try_Traverse);
 BENCHMARK(Pointer_Push_Back_Pointer_To_Weak_Pointer);
 BENCHMARK(Pointer_Walker_Schema_ISO_Language);
 BENCHMARK(Pointer_Maybe_Tracked_Deeply_Nested)->Arg(0)->Arg(1);
 BENCHMARK(Pointer_Position_Tracker_Get_Deeply_Nested);
+BENCHMARK(Schema_Tracker_ISO_Language);
+BENCHMARK(Schema_Tracker_ISO_Language_To_JSON);
